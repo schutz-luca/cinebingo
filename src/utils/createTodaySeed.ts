2b2a23b041910bf0d8c43d7c07c2166cfa5c2e6e
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-03-11 21:29:35 -0300

    use locale date to create seed

--- src/utils/createTodaySeed.ts
+++ src/utils/createTodaySeed.ts
@@ -1,1 +1,6 @@
-export const createTodaySeed = () => parseInt(new Date().toISOString().split('T')[0].replace(/-/g, ''), 10);
+export const createTodaySeed = () => {
+    const localDate = new Date();
+    const offset = localDate.getTimezoneOffset() * 60000; // Convert offset to milliseconds
+    const todayLocal = new Date(localDate.getTime() - offset);
+    return parseInt(todayLocal.toISOString().split('T')[0].replace(/-/g, ''), 10);
+}
