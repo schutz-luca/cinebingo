798ac35fcabeeea040c93060d88f4d1f067993e3
schutz-luca <lucaschutzenhofer@hotmail.com>
2025-04-15 12:56:20 -0300

    fix undefined case on sortRanking

--- src/utils/sortRanking.ts
+++ src/utils/sortRanking.ts
@@ -2,7 +2,7 @@ import { Player } from '../lib/fluid-framework/types';
 
 export const sortRanking = (players: Player[]) =>
     players.sort((a, b) => {
-        if (b.points !== a.points) {
+        if (b.points !== a.points || (!a.tries || !b?.tries)) {
             return b.points - a.points; // Sort by 'points' in DESC order
         }
         return a.tries - b.tries; // If 'points' is the same, sort by 'tries' in ASC order
